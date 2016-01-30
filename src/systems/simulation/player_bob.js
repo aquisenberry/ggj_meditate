8fb7fb326b2dccc3b1878ecf29de253a9295d4bc
Anthony Quisenberry <anthony.quisenberry01@gmail.com>
2016-01-30 17:58:47 -0500

    add player bob

---
+++ src/systems/simulation/player_bob.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = function(ecs, data) {
+    ecs.addEach(function(entity, elapsed) {
+
+        var image = data.entities.get(entity, "image");
+        var time = data.entities.get(entity,"time");
+        image.destinationY = Math.sin(time)*5;
+        data.entities.set(entity, "time", time + .009);
+    }, "player");
+}
