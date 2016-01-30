0217c87f10c283674677ae82eb6d55cac72430d5
Anthony Quisenberry <anthony.quisenberry01@gmail.com>
2016-01-30 18:01:29 -0500

    adjust bob

--- src/systems/simulation/player_bob.js
+++ src/systems/simulation/player_bob.js
@@ -5,7 +5,7 @@ module.exports = function(ecs, data) {
 
         var image = data.entities.get(entity, "image");
         var time = data.entities.get(entity,"time");
-        image.destinationY = Math.sin(time)*5;
+        image.destinationY = Math.sin(time)*7;
         data.entities.set(entity, "time", time + .009);
     }, "player");
 }
