f3c034d7e7e34f7b0d9fc4bd0ad1cb5eb4552b30
Matthew Caldwell <caldwellysr@gmail.com>
2016-01-30 00:03:50 -0500

    Set projectile positions to the edge of the screen

--- src/scripts/spawn_projectiles.js
+++ src/scripts/spawn_projectiles.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function cast_to_edge(x, y, pos) {
+function cast_to_edge(x, y, pos, data) {
    var d = Math.sqrt((pos.x - x) * (pos.x - x) + (pos.y - y) * (pos.y - y)); 
    var uv = {
        "x": (x-pos.x)/d,
@@ -11,6 +11,9 @@ function cast_to_edge(x, y, pos) {
        x+=uv.x;
        y+=uv.y;
    } while( x >= 0 && x <= data.canvas.width && y >= 0 && y <= data.canvas.height );
+
+   return {"x": x, "y": y}
+
 }
 
 module.exports = function(entity, data) {
@@ -21,7 +24,9 @@ module.exports = function(entity, data) {
 
     var projectile = data.instantiatePrefab("projectile");
 
-    cast_to_edge(x, y, constants.center);
+    var new_pos = cast_to_edge(x, y, constants.center, data);
+
+    data.entities.set(projectile, "position", new_pos);
 
     var timers = data.entities.get(entity, "timers");
     timers.spawn_projectile.time = 0;
