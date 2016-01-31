973d9da5b61d0e57ac58a51a31a1bab6bfc962bb
Matthew Caldwell <caldwellysr@gmail.com>
2016-01-30 23:17:42 -0500

    negative and positive images

--- src/scripts/spawn_projectiles.js
+++ src/scripts/spawn_projectiles.js
@@ -33,6 +33,14 @@ module.exports = function(entity, data) {
     var projectile = data.instantiatePrefab("projectile");
     var projectile_size = data.entities.get(projectile,"size");
 
+    var negative = Math.floor(Math.random() * 9) % 2;
+    data.entities.set(projectile, "negative_effect", negative);
+    if(negative) {
+        data.entities.set(projectile, "image", {"name": "negative_projectile"});
+    } else {
+        data.entities.set(projectile, "image", {"name": "positive_projectile"});
+    }
+
     var new_pos = cast_to_edge(projectile_size,x, y, constants.center, data);
     new_pos.x = new_pos.x + projectile_size.width/2;
     new_pos.y = new_pos.y + projectile_size.height/2;
---
+++ src/systems/simulation/projectile_rotation.js
@@ -0,0 +1,6 @@
+"use strict";
+
+module.exports = function(ecs, data) {
+    ecs.addEach(function rotate_projectiles(entity, elapsed) {
+    }, "projectile");
+}
