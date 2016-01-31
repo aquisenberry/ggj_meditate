77a8f29a816bce119274ad68e46e8c8283a8e44f
Matthew Caldwell <caldwellysr@gmail.com>
2016-01-31 13:40:49 -0500

    render zen grenade

--- src/systems/renderer/render_zengrenade.js
+++ src/systems/renderer/render_zengrenade.js
@@ -4,7 +4,12 @@ module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
 	ecs.addEach(function(entity, context) { // eslint-disable-line no-unused-vars
         var position = data.entities.get(entity, "position");
 		var size = data.entities.get(entity, "size");
+        context.globalAlpha = 0.6;
 		context.fillStyle = "red"; 
-		context.fillRect(position.x, position.y, size.width, size.height);
+        context.beginPath();
+		context.arc(position.x + size.width/2, position.y+size.height/2, size.width/2, 0, Math.PI*2);
+        context.closePath();
+        context.fill();
+        context.globalAlpha = 1;
     }, "zengrenade");
 }
--- src/systems/simulation/zengrenade.js
+++ src/systems/simulation/zengrenade.js
@@ -1,13 +1,31 @@
 "use strict";
 
+function distance( pos1, pos2 ) {
+    return Math.sqrt((pos1.x - pos2.x) * (pos1.x - pos2.x) + (pos1.y - pos2.y) * (pos1.y - pos2.y)); 
+}
+
 module.exports = function(ecs, data) {
     ecs.addEach(function zengrenade(entity, elapsed) {
-
+        
+        var player_pos = data.entities.get(entity, "position");
+        var player_radius = data.entities.get(entity, "size").width / 2;
+        var player_center = {
+            "x": player_pos.x + player_radius,
+            "y": player_pos.y + player_radius
+        };
         var entity_collisions = data.entities.get(entity, "collisions");
         for(var i = 0; i < entity_collisions.length; i++) {
             if(data.entities.get(entity_collisions[i], "projectile")) {
-                data.entities.destroy(entity_collisions[i--]);
-            } 
+                var proj_pos = data.entities.get(entity_collisions[i], "position");
+                var proj_radius = data.entities.get(entity_collisions[i], "size").width / 2;
+                var proj_center = {
+                    "x": proj_pos.x + proj_radius,
+                    "y": proj_pos.y + proj_radius
+                };
+                if( distance(player_center, proj_center) <= (player_radius + proj_radius) ) {
+                    data.entities.destroy(entity_collisions[i--]);
+                } 
+            }
         }
                      
     }, "zengrenade");
