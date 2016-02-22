5502cb1b7d0ef9d023f06f9e991eddac63cd3add
Matthew Caldwell <mcaldwell@mediamarketers.com>
2016-02-22 14:13:24 -0500

    tutorial scene complete*

--- src/systems/renderer/render_tutorial_text.js
+++ src/systems/renderer/render_tutorial_text.js
@@ -1,14 +1,18 @@
 "use strict";
 
 module.exports = function(ecs, data) {
-	if(data.arguments.mode == "tutorial") {
-		ecs.addEach(function(entity, context) { // eslint-disable-line no-unused-vars
+    ecs.addEach(function(entity, context) { // eslint-disable-line no-unused-vars
 
-			// Positive Projectile Position: {"x": data.canvas.width * 0.25, "y": data.canvas.height * 0.1}
-			context.font = "20px Arial";
-			context.fillStyle = "black";
-			context.fillText("This is Positive Energy. Let it make it to your monk.", data.canvas.width * 0.1, data.canvas.height * 0.2);
+        var pos_txt = "This is Positive Energy. It will bring your monk to zen";
+        var neg_txt = "This is Negative Energy. It will ruin your monk's concentration.";
+        var zengrenade_txt = "Press space to drop a zen grenade.";
 
-		}, "camera");
-	}
+        // Positive Projectile Position: {"x": data.canvas.width * 0.25, "y": data.canvas.height * 0.1}
+        context.font = "20px Arial";
+        context.fillStyle = "black";
+        context.fillText(pos_txt, data.canvas.width * 0.25 - (context.measureText(pos_txt).width / 2), data.canvas.height * 0.2);
+        context.fillText(neg_txt, data.canvas.width * 0.75 - (context.measureText(neg_txt).width / 2), data.canvas.height * 0.2);
+        context.fillText(zengrenade_txt, data.canvas.width * 0.5 - (context.measureText(zengrenade_txt).width / 2), data.canvas.height * 0.85);
+
+    }, "camera");
 }
--- src/systems/simulation/mouse_simulation.js
+++ src/systems/simulation/mouse_simulation.js
@@ -21,6 +21,10 @@ module.exports = function(ecs, data) {
         var entity_collisions = data.entities.get(entity, "collisions");
         if(data.input.mouse.consumePressed(0)) {
             for(var i = 0; i < entity_collisions.length; ++i) {
+                if(data.entities.get(entity_collisions[i], "name") == "play_button") {
+                    data.entities.set(entity_collisions[i], "image", {"name": "play_pressed"}); 
+                    data.switchScene("main");
+                }
                 if(data.entities.get(entity_collisions[i], "projectile") && !data.entities.get(entity_collisions[i], "negative_effect")) {
                     var vel = data.entities.get(entity_collisions[i],"velocity");
                     var col_timers = data.entities.get(entity_collisions[i],"timers");
