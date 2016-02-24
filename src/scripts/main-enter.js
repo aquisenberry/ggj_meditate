5f8f2d407129861e4097edbad93698f848e9e7fd
Matthew Caldwell <caldwellysr@gmail.com>
2016-02-24 18:00:18 -0500

    only spawn clouds on level 1

--- src/scripts/main-enter.js
+++ src/scripts/main-enter.js
@@ -2,4 +2,12 @@
 
 module.exports = function(data) { // eslint-disable-line no-unused-vars
 
+	var camera = 0;
+	var camera_timers = data.entities.get(camera, "timers");
+	var level = data.arguments.level;
+
+	if(level == 1) {
+		camera_timers.spawn_clouds.running = true;
+	}
+	
 };
