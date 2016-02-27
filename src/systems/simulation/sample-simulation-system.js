a7de59c6ebf4053b72b78c9c805ef0db65afec5e
Matthew Caldwell <caldwellysr@gmail.com>
2016-02-27 08:43:08 -0500

    explosion to clear screen

--- src/systems/simulation/sample-simulation-system.js
+++ src/systems/simulation/sample-simulation-system.js
@@ -1,6 +1,6 @@
 "use strict";
 
-module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
+module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
 	ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
         var entity_collisions = data.entities.get(entity, "collisions");
 		for(var i = 0; i < entity_collisions.length; ++i) {
