4aff21496f9f1e07cea23becaaa4f2ca455c1338
Anthony Quisenberry <anthony.quisenberry01@gmail.com>
2016-03-01 20:56:12 -0500

    pulse om

--- src/systems/renderer/render_zen.js
+++ src/systems/renderer/render_zen.js
@@ -6,12 +6,14 @@ module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
 		var position = data.entities.get(entity, "position");
 		var size = data.entities.get(entity, "size");
 		var om_progress = data.entities.get(entity,"om_progress");
-		context.strokeStyle = "#82d1e1"
-		context.lineWidth = size.width*0.05;
-		context.globalAlpha = 1;
-		context.beginPath();
-		context.arc(position.x+size.width/2,position.y+size.height/2,size.width/2- size.width*0.02,0,Math.PI*2*om_progress.value/om_progress.max);
-		context.stroke();
-		context.globalAlpha = 1;
+		if(om_progress.value <= om_progress.max){
+			context.strokeStyle = "#82d1e1"
+			context.lineWidth = size.width*0.05;
+			context.globalAlpha = 1;
+			context.beginPath();
+			context.arc(position.x+size.width/2,position.y+size.height/2,size.width/2- size.width*0.02,0,Math.PI*2*om_progress.value/om_progress.max);
+			context.stroke();
+			context.globalAlpha = 1;
+		}
 	}, "om");
 };
---
+++ src/systems/simulation/pulse_om.js
@@ -0,0 +1,36 @@
+module.exports = function(ecs, data) {
+    ecs.addEach(function(entity, elapsed) {
+    	if(data.entities.get(entity,"image")){
+    		var half_bob_range = 1;
+	        var time_incriment = 0.009;
+
+	        var size = data.entities.get(entity, "size");
+	        var base_size = data.entities.get(entity, "base_size");
+	        var time = data.entities.get(entity,"time");
+	        var position = data.entities.get(entity, "position");
+
+	        size.width = (Math.sin(time.scale_time)*(1/12)+ 1) *base_size.width ;
+	        size.height = (Math.sin(time.scale_time)*(1/12)+ 1) *base_size.height;
+    		console.log(size);
+	        time.scale_time += time_incriment; 
+    	}
+        
+    }, "halo");
+    ecs.addEach(function(entity, elapsed) {
+    	if(data.entities.get(9,"image")){
+    		var half_bob_range = 1;
+	        var time_incriment = 0.009;
+
+	        var size = data.entities.get(entity, "size");
+	        var base_size = data.entities.get(entity, "base_size");
+	        var time = data.entities.get(entity,"time");
+	        var position = data.entities.get(entity, "position");
+
+	        size.width = (Math.sin(time.scale_time)*(1/12)+ 1) *base_size.width ;
+	        size.height = (Math.sin(time.scale_time)*(1/12)+ 1) *base_size.height;
+    		console.log(size);
+	        time.scale_time += time_incriment; 
+    	}
+        
+    }, "om");
+}
